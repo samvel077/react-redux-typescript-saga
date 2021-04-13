0172dbd9ba6b22f619008b1a5457308952a3f430
Samvel <samvelhakobyan215@gmail.com>
2021-04-13 15:50:02 +0400

    added wait function

--- src/saga/todoSaga.ts
+++ src/saga/todoSaga.ts
@@ -3,24 +3,28 @@ import axios from "axios";
 import { put, takeEvery, call, select } from "redux-saga/effects";
 import { TodoActionTypes } from "../types/todo";
 
-let state: RootState
+let state: RootState;
 
 function* getState() {
-   state = yield select((state: RootState) => state);
+  state = yield select((state: RootState) => state);
 }
 
+// const wait = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
 const axiosTodofromApi = () =>
-axios.get("https://jsonplaceholder.typicode.com/todos", {
-  params: { _page: state?.todo?.page, _limit: state?.todo?.limit },
-});
+  axios.get("https://jsonplaceholder.typicode.com/todos", {
+    params: { _page: state?.todo?.page, _limit: state?.todo?.limit },
+  });
 
 function* todoCall(): Generator {
   try {
-    yield getState()
+    yield getState();
 
     yield put({ type: TodoActionTypes.FETCH_TODOS });
-
+    
     const data: any = yield call(axiosTodofromApi);
+    
+    // yield wait(500);
 
     yield put({
       type: TodoActionTypes.FETCH_TODOS_SUCCESS,
